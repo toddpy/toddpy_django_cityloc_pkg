e4d137ea096dd2ce6c068a5991811398cd64f95a
toddpy <nucamp.co@sparrowmail.net>
2021-11-12 23:38:31 -0500

    update docs

--- citylocations/views.py
+++ citylocations/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def loc_nyc(request):
-    """ Display the content for the NYC location """
+    """ 
+    Display the content for the NYC location 
+    
+    """
+
     return render(request, 'citylocations/loc_nyc.html', {})
 
